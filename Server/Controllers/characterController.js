915507151631ebff1af06a4c990c068577e87c08
FarmerSamuel-FS <sfarmer1@student.fullsail.edu>
2024-05-15 14:00:34 -0400

    Back-End Complete

--- Server/Controllers/characterController.js
+++ Server/Controllers/characterController.js
@@ -1,41 +1,6 @@
 // characterController.js
-
-const Character = require('../models/Character');
 const SubmittedCharacter = require('../models/SubmittedCharacter');
 
-// Controller function to fetch all characters
-exports.getAllCharacters = async (req, res) => {
-  try {
-    const characters = await Character.find();
-    res.json(characters);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-// Controller function to fetch a character by ID
-exports.getCharacterById = async (req, res) => {
-  try {
-    const character = await Character.findById(req.params.id);
-    if (!character) {
-      return res.status(404).json({ error: 'Character not found' });
-    }
-    res.json(character);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-// Controller function to search characters by name
-exports.searchCharactersByName = async (req, res) => {
-  try {
-    const searchTerm = req.query.name;
-    const characters = await Character.find({ name: { $regex: new RegExp(searchTerm, 'i') } });
-    res.json(characters);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
 
 // Controller function to submit a character
 exports.submitCharacter = async (req, res) => {
@@ -55,7 +20,7 @@ exports.submitCharacter = async (req, res) => {
       episode,
     });
 
-    // Save the new character submission
+ 
     await newCharacter.save();
     
     res.json({ message: 'Character submitted successfully' });
